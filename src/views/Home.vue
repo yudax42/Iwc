86808fbf66e41b65e4d563f64aee30fdae0d72c2
yudaxx <yudax42@gmail.com>
2021-01-22 19:33:08 +0100

    displaying channels done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,53 @@
     <form @submit.prevent>
       <input type="file" @change="preview" />
     </form>
-    <pre>
-      {{ event }}
-      {{ playlist }}
-    </pre>
+    <div class="container">
+      <div class="playlists">
+        <div v-show="playlist.length > 1">
+          <div class="channel" v-for="i in playlist" :key="i.link">
+            <p>{{ i.name }}</p>
+            <!-- <p>{{ i.link }}</p> -->
+          </div>
+        </div>
+      </div>
+      <div class="player"></div>
+    </div>
   </div>
 </template>
+
+<style lang="css" scoped>
+.container {
+  width: 100%;
+  overflow: hidden;
+  margin-top: 10%;
+}
+.container div {
+  display: inline-block;
+}
+.playlists {
+  /* background: red; */
+  width: 29%;
+  /* height: 20px; */
+  float: left;
+}
+.playlists .channel {
+  background: #eee;
+  border: 0.5px solid #333;
+  margin-bottom: 5px;
+  padding: 10px;
+  margin: 10px;
+  width: 100%;
+  display: block;
+}
+.playlists .channel:hover {
+  cursor: pointer;
+}
+.player {
+  background: blue;
+  width: 70%;
+  height: 20px;
+}
+</style>
 
 <script>
 export default {
@@ -35,11 +76,11 @@ export default {
       // console.log(parse);
       while (i < parse.length) {
         let j = i;
-        const channel = { name: parse[i].split("|")[1], link: parse[j + 1] };
+        const channel = { name: parse[i].split(",")[1], link: parse[j + 1] };
         playlist.push(channel);
         i = i + 2;
       }
-      console.log(playlist);
+      this.playlist = playlist;
     },
   },
 };
