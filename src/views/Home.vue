e24e6d2b6b76f946f7fe7366787af182c855b491
yudaxx <yudax42@gmail.com>
2021-01-22 19:21:04 +0100

    parsing done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
     </form>
     <pre>
       {{ event }}
+      {{ playlist }}
     </pre>
   </div>
 </template>
@@ -14,14 +15,31 @@ export default {
   data() {
     return {
       event: "",
+      playlist: "",
     };
   },
   methods: {
     preview(ev) {
       const file = ev.target.files[0];
       const reader = new FileReader();
-      reader.onload = (e) => (this.event = e.target.result);
-      reader.readAsText(file);
+      reader.readAsText(file, "utf-8");
+      reader.onload = () => {
+        this.event = reader.result;
+        this.process();
+      };
+    },
+    process() {
+      let parse = this.event.split("\n");
+      let i = 1;
+      let playlist = [];
+      // console.log(parse);
+      while (i < parse.length) {
+        let j = i;
+        const channel = { name: parse[i].split("|")[1], link: parse[j + 1] };
+        playlist.push(channel);
+        i = i + 2;
+      }
+      console.log(playlist);
     },
   },
 };
